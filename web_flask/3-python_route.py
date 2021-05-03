3b3ca2ae28415330b32c8a10a10f16c49e9dc4c8
tomi1710 <2294@holbertonschool.com>
2021-05-03 00:58:32 +0000

    fix

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Made by Tomas de Castro for Holberton School 2021 """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Returns a specific string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB_world():
+    """ Returns a specific string """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_world(text):
+    """ Returns all with c """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def Python_world(text='is cool'):
+    """ Returns all with Python """
+    return 'Python %s' % text.replace('_', ' ')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
