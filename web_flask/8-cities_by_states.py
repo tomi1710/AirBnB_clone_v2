02b8e584a4450cca940f5e3ec81968d2cdd42e26
tomi1710 <2294@holbertonschool.com>
2021-05-03 02:11:55 +0000

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,20 +5,71 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ display a HTML page """
-    s_list = storage.all(State).values()
-    return render_template("8-cities_by_states.html", s_list=s_list)
+@app.route('/')
+def hello_world():
+    """ Returns a specific string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNB_world():
+    """ Returns a specific string """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def C_world(text):
+    """ Returns all with C """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def Python_world(text='is cool'):
+    """ Returns all with Python """
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def integer_world(n):
+    """ Returns all with number """
+    return ("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Returns number in a html document """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def oddeven_template(n):
+    """ Returns number in a html page """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+@app.route('/states_list')
+def State_world():
+    """ List of all states in the database """
+    list_states = storage.all(State).values()
+    return render_template('7-states_list.html', list_states=list_states)
+
+
+@app.route('/cities_by_states')
+def Cities_World():
+    """ List of cities from State in the database """
+    list_states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', list_states=list_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ remove SQLAlchemy Session """
+def Down_World(exception):
+    """ Killing the sql session """
     storage.close()
 
+
 if __name__ == '__main__':
-    """ listening on 0.0.0.0 """
     app.run(host='0.0.0.0', port=5000)
